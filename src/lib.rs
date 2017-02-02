5ae2a5522fde6dd54adca2d3fee183556761c92d
Glenn Pierce <glenn.pierce@carnegosystems.com>
2017-02-02 14:46:15 +0000

    Added example elements. Need to connect them

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,30 @@ use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 use std::thread;
 use std::collections::HashMap;
+use std::io;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+
+#[derive(Debug)]
+pub enum ElementError {
+    FAILED_TO_READ_INPUT,
+}
+
+pub type ElementResult<T> = Result<T, Element>;
 
 enum ElementPadType {
     STRING,
     NUMERIC,
     DATETIME,
-    FILEPATH
+    FILE
 }
 
 struct ElementPad {
     name : String,
     pad_type: ElementPadType,
-    comm : (Sender<i32>, Receiver<i32>),
+    //comm : (Sender<i32>, Receiver<i32>),
 }
 
 impl ElementPad {
@@ -21,22 +33,28 @@ impl ElementPad {
         ElementPad{
             name : name,
             pad_type : pad_type,
-            comm : mpsc::channel(),
+            //comm : mpsc::channel(),
         }
     }
+
+    // pub fn send_on_output(&self, index : u8) {
+    //     //let mut map = self.inputs; 
+    //     self.outputs[index].comm[0].send(id).unwrap();
+    // }
 }
 
 struct BaseElement{
     name : String,
     inputs : Vec<ElementPad>,
-    //output : ElementPad,
+    outputs : Vec<ElementPad>,
 }
 
 impl BaseElement {
     pub fn new(name: String) -> Self {
         BaseElement{
             name : name,
-            inputs : Vec::new()
+            inputs : Vec::new(),
+            outputs : Vec::new(),
         }
     }
 
@@ -44,26 +62,79 @@ impl BaseElement {
         //let mut map = self.inputs; 
         self.inputs.push(ElementPad::new (name, pad_type));
     }
+
+    pub fn add_output(&mut self, name : String, pad_type : ElementPadType) {
+        //let mut map = self.inputs; 
+        self.outputs.push(ElementPad::new (name, pad_type));
+    }
 }
 
 trait Element {
-    fn new(name: String) -> Self;
+    fn initalise(&mut self);
     fn run(&self);
 }
 
 struct FileSourceElement {
     base : BaseElement,
+    file: File,
+}
+
+impl FileSourceElement {
+    fn new(name: String, file : File) -> Self {
+        FileSourceElement { base : BaseElement::new(name),
+                            file :file }
+    }
 }
 
 impl Element for FileSourceElement {
-    fn new(name: String) -> Self {
-        FileSourceElement { base : BaseElement::new(name) }
+    
+    fn initalise(&mut self) {
+        //self.base.add_input("input_pad1_filesource".to_string(), ElementPadType::FILE);
+        self.base.add_output("output_pad_filedata".to_string(), ElementPadType::FILE);
     }
 
     fn run(&self) {
-        println!("Hi");
+        // Read from file and stream data to the output pad.
+        let mut reader = BufReader::new(&self.file);
+
+        let (tx, rx): (Sender<String>, _) = mpsc::channel();
+        for line in reader.lines() {
+            let l = line.unwrap();
+            //self.base.outputs[0].comm
+            //println!("{}", l); 
+            tx.send(l).unwrap();
+        }           
     }
 }
+
+
+
+struct ConsoleEchoElement {
+    base : BaseElement,
+}
+
+impl ConsoleEchoElement {
+    fn new(name: String, file : File) -> Self {
+        ConsoleEchoElement { base : BaseElement::new(name) }
+    }
+}
+
+impl Element for ConsoleEchoElement {
+    
+    fn initalise(&mut self) {
+        // TODO needs multiple types
+        self.base.add_input("input_pad1_filesource".to_string(), ElementPadType::FILE);
+    }
+
+    fn run(&self) {
+        let (_, rx): (_, Receiver<String>) = mpsc::channel();
+        println!("{}", rx.recv().unwrap());       
+    }
+}
+
+
+
+
 
 #[cfg(test)]
 mod tests {
@@ -72,7 +143,20 @@ mod tests {
 
         use ::Element;
         use ::FileSourceElement;
+        use std::fs::File;
 
-        let file_source_element = FileSourceElement::new("My Source".to_string());
+        match File::open("data/test.txt") {
+            Ok(file) => {
+              let file_source_element = FileSourceElement::new("My Source".to_string(), file);
+              file_source_element.run();
+            },
+            Err(e) => {
+                // fallback in case of failure.
+                // you could log the error, panic, or do anything else.
+                println!("{}", e);
+            }
+        };
+
+        
     }
 }
